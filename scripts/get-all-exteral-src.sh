24f414b5eddcb6b6ec1c7da975021b92e16ef12b
Ryoji Kamei <ryoji.kamei@gmail.com>
2014-05-15 11:31:55 +0900

    Modify for busybox's wget

--- scripts/get-all-exteral-src.sh
+++ scripts/get-all-exteral-src.sh
@@ -16,15 +16,15 @@ for s in `ls $PROG_DIR/0*.sh`; do
 		for u in $src; do
 			num=`echo $u | cut -f3 -d_ | cut -f1 -d=`
 			SRC=`eval echo $u | cut -f2 -d=`
-			FILE=$OPKG_WORK_SOURCES/$NAME/$SRC
+			DIR=$OPKG_WORK_SOURCES/$NAME
+			FILE=$DIR/$SRC
 
 			if [ -f $FILE ]; then
 				echo "SKIP: $SRC is found."
 			else
 				eval URL=`grep ^EXTERNAL_URL_$num $s | cut -f2 -d=`
 				echo -n "GET: $URL/$SRC... "
-				mkdir -p $OPKG_WORK_SOURCES/$NAME
-				wget -q -t 10 -c $URL/$SRC -O $FILE
+				mkdir -p $DIR && wget -q -c $URL/$SRC -P $DIR
 				if [ $? -eq 0 ]; then
 					echo "done"
 				else
